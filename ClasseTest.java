2b142a27ef8c6525c2744b82a7cb85e363811a3a
Saymu <41518761+Saymu@users.noreply.github.com>
2021-05-29 18:29:14 +0200

    another remote commit _ conflict

--- ClasseTest.java
+++ ClasseTest.java
@@ -3,7 +3,7 @@ package com.git;
 public class ClasseTest {
 
     public static void main(String[] args) {
-        System.out.println("HOPE");
+        System.out.println("HOPE_remote_change");
 		System.out.println("I feel alive");
     }
 }
