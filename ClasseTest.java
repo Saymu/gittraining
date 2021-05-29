254d86fa3f3c29118a3c1a3d6bbf96267d8d994c
Mohammed Choukrani <choukrani.med.90@gmail.com>
2021-05-29 18:29:57 +0200

    another local commit

--- ClasseTest.java
+++ ClasseTest.java
@@ -3,7 +3,7 @@ package com.git;
 public class ClasseTest {
 
     public static void main(String[] args) {
-        System.out.println("HOPE");
+        System.out.println("HOPE_local_change");
 		System.out.println("I feel alive");
     }
 }
