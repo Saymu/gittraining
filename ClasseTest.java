5456d30a353461f15d30b0bf896ab5ec15f0ee57
Mohammed Choukrani <choukrani.med.90@gmail.com>
2021-05-30 13:03:32 +0200

    second local branch commit

--- ClasseTest.java
+++ ClasseTest.java
@@ -1,6 +1,9 @@
 package com.git;
 
 public class ClasseTest {
+	
+	public void doThings(){
+	}
 
     public static void main(String[] args) {
         System.out.println("HOPE_local_branch_change");
